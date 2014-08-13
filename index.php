f3e6c0cd644db9635ead5523cb13b812a3c2e1c7
deB4SH <github@b4sh.de>
2014-08-13 12:36:15 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css"/>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        
+    </head>
+    <body>
+        
+        <div class="container" role="navigation">
+            <div class="row">
+                <div class="col-md-12 navigation-line">
+                    BiTCLiCKER
+                </div>         
+            </div>
+        </div>
+        
+        <div class="container theme-showcase" role="main">
+            <div class="row">
+                <div class="col-md-3">col-md-3</div>         
+                <div class="col-md-6 center">col-md-6</div>
+                <div class="col-md-3">col-md-3</div>
+            </div>
+        </div>
+             
+        
+    </body>
+</html>
+
+
+
+
+
+
