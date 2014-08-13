8aca6bfb1969fbf71dabbd42ce34584a6b1c654c
deB4SH <github@b4sh.de>
2014-08-13 13:21:11 +0200

    NEW: Boxes and bootstrap design elements

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     </head>
     <body>
         
-        <div class="container" role="navigation">
+        <div class="container" role="headline">
             <div class="row">
                 <div class="col-md-12 navigation-line">
                     BiTCLiCKER
@@ -26,9 +26,53 @@
         
         <div class="container theme-showcase" role="main">
             <div class="row">
-                <div class="col-md-3">col-md-3</div>         
+                <div class="col-md-3">
+                    
+                    <div class="col-md-3-box">
+                        
+                        <div class="panel panel-success">
+                            <div class="panel-heading col-md-3-box-heading">
+                                <h3 class="panel-title">
+                                    Panel title
+                                </h3>
+                            </div>
+                            <div class="panel-body">
+                              Panel content
+                            </div>
+                        </div>
+                        
+                    </div>
+                    
+                   
+                </div>         
                 <div class="col-md-6 center">col-md-6</div>
-                <div class="col-md-3">col-md-3</div>
+                <div class="col-md-3">
+                    
+                    <div class="col-md-3-box">
+                        <div class="panel panel-default">
+                            <div class="panel-heading col-md-3-box-heading">
+                              <h3 class="panel-title">Bit.Crate</h3>
+                            </div>
+                            <div class="panel-body">
+                                
+                              0 Bits per secound :(
+                            </div>
+                        </div>
+                    </div>
+                        
+                    <div class="col-md-3-box">´
+                        <div class="panel panel-default">
+                            <div class="panel-heading col-md-3-box-heading">
+                              <h3 class="panel-title">Navigation</h3>
+                            </div>
+                            <div class="panel-body">
+                                <li>Home</li>
+                                <li>Settings</li>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
              
