8bc026f39f6fe7b7c2d855c3cb2faa488261785e
deB4SH <github-mail@b4sh.de>
2014-08-25 19:35:15 +0200

    dart is so damn comfy :)

--- web/assets/dart/models.dart
+++ web/assets/dart/models.dart
@@ -14,9 +14,59 @@ class Wallet{
     _amount = _amount + _clickMultiplier;
   }
   
-  double getAmount(){
-    return _amount;
+  void addIncome(double amount){
+    _amount = amount;
   }
   
+  double getAmount(){
+    return _amount;
+  } 
+}
+
+class Building{
+  Wallet _walletObj;
+  
+  String _name;
+  String _description;
+  double _basicIncome;
+  double _incomeMultiplier = 1.0;
+  
+  int _level = 0;
+  double _levelupCosts = 0;
+  double _levelupCostsMultiplier;
+  
+  Building(String buildingName, String buildingDesc, double buildingIncome, double levelupCosts, double levelupMultiplier, Wallet wallet){
+    this._name = buildingName;
+    this._description = buildingDesc;
+    this._basicIncome = buildingIncome;
+    
+    this._levelupCosts = levelupCosts;
+    this._levelupCostsMultiplier = levelupMultiplier;
+    
+    this._levelupCosts = levelupCosts;
+    this._levelupCostsMultiplier = levelupMultiplier;
+    
+    this._walletObj = wallet;
+  }
+  
+  double getIncome(){
+    return this._basicIncome * this._incomeMultiplier;
+  }
+  
+  String getName(){
+    return this._name;
+  }
+  
+  String getDescription(){
+    return this._description;
+  }
+  
+  void generateIncome(){
+    _walletObj.addIncome(getIncome() * _level);
+  }
+  
+  void levelUp(){
+    
+  }
   
 }
--- web/bitclicker.dart
+++ web/bitclicker.dart
@@ -1,6 +1,7 @@
 library bitclicker;
 
 import 'dart:html';
+import 'dart:async';
 part 'assets/dart/models.dart';
 
 /*
@@ -8,17 +9,34 @@ part 'assets/dart/models.dart';
  */ 
 
 Wallet bitWallet;
+List<Building> building;
+
+Timer timer = new Timer(const Duration(seconds: 1), incomeGeneration());
 
 
-//initialises the game
 void setup(){
   bitWallet = new Wallet(0.0, 0.0);
+  building = new List<Building>();
+  //set up buildings
+  //(String buildingName, String buildingDesc, double buildingIncome, double levelupCosts, double levelupMultiplier, Wallet wallet)
+  building.add(new Building("Hand", "A virtual Mouse that clicks the Bit", 1.0, 10.0, 0.5, bitWallet));
+  building.add(new Building("Discette", "An old datastorage to keep small dirty images", 1.0, 10.0, 0.5, bitWallet));
+  building.add(new Building("CD", "Well, this stores some old school holiday movies ;)", 1.0, 10.0, 0.5, bitWallet));
+  building.add(new Building("Harddrive", "Store all dem Games!", 1.0, 10.0, 0.5, bitWallet));
+  building.add(new Building("Mynecraft", "Host a Mynecraft Server for friends!", 1.0, 10.0, 0.5, bitWallet));
+  
+  
+  //setup mouse binding and keyboard binding
+  querySelector(".bitbutton").onClick.listen(clickBit);
 }
 
 void main() {
   setup();
-  querySelector(".bitbutton")
-      ..onClick.listen(clickBit);
+  
+}
+
+void incomeGeneration(){
+  
 }
 
 
